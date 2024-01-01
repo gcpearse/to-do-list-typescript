6b6316581c59dffc2fb4a9644e9e1c7478389f71
gcpearse <glen.c.pearse@gmail.com>
2024-01-01 18:47:34 +0000

    creates [task, setTask] state and passes props to Input

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,15 @@
+import { useState } from 'react'
 import './App.css'
 import Input from './components/Input'
 
 const App: React.FC = () => {
+
+  const [task, setTask] = useState<string>("")
+
   return (
     <>
       <h1>Sortmin</h1>
-      <Input />
+      <Input task={task} setTask={setTask} />
     </>
   )
 }
--- src/components/Input.tsx
+++ src/components/Input.tsx
@@ -1,12 +1,19 @@
 import './styles.css'
 
-const Input = () => {
+interface Props {
+  task: string
+  setTask: React.Dispatch<React.SetStateAction<string>>
+}
+
+const Input: React.FC<Props> = ({ task, setTask }) => {
   return (
     <form className='input-form'>
       <input
         type='text'
         className='input-field'
-        placeholder='Enter a task' />
+        placeholder='Enter a task'
+        value={task}
+        onChange={(event) => setTask(event.target.value)} />
       <button
         type='submit'
         className='input-btn'>
