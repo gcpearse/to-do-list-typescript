dc7e387f7b2bdabd0f1bb201ce89b837e92e8b4d
gcpearse <glen.c.pearse@gmail.com>
2024-01-01 19:11:14 +0000

    builds handleSubmit functionality for form submission

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,31 @@
 import { useState } from 'react'
 import './App.css'
 import Input from './components/Input'
+import { Task } from './model'
 
 const App: React.FC = () => {
 
-  const [task, setTask] = useState<string>("")
+  const [input, setInput] = useState<string>("")
+  const [id, setId] = useState<number>(0)
+  const [tasks, setTasks] = useState<Task[]>([])
+
+  const handleSubmit = (event: React.FormEvent) => {
+    event.preventDefault()
+    if (input) {
+      setId(id + 1)
+      setTasks([...tasks, {
+        id: id,
+        task: input,
+        isComplete: false
+      }])
+      setInput("")
+    }
+  }
 
   return (
     <>
       <h1>Sortmin</h1>
-      <Input task={task} setTask={setTask} />
+      <Input input={input} setInput={setInput} handleSubmit={handleSubmit} />
     </>
   )
 }
