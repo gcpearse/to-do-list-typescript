0eebe5b91bd08198df913349a3b1b466f6328c23
gcpearse <glen.c.pearse@gmail.com>
2024-01-02 01:30:07 +0000

    adds functionality to allow user to clear the entire list - user is prompted to confirm this action

--- src/components/TaskList.tsx
+++ src/components/TaskList.tsx
@@ -1,3 +1,4 @@
+import { useState } from 'react'
 import { Task } from '../model'
 import SingleTask from './SingleTask'
 import './styles.css'
@@ -8,11 +9,45 @@ interface Props {
 }
 
 const TaskList: React.FC<Props> = ({ tasks, setTasks }) => {
+
+  const [confirm, setConfirm] = useState<boolean>(false)
+
   return (
     <div className='tasks'>
       {tasks.map((task) => {
         return <SingleTask key={task.id} task={task} tasks={tasks} setTasks={setTasks} />
       })}
+      {tasks.length ? (
+        <button
+          className='clear-btn'
+          onClick={() => setConfirm(true)}>
+          Clear list
+        </button>
+      ) : (
+        null
+      )}
+      {confirm ? (
+        <div className='confirm-section'>
+          <p>Are you sure you want to clear the list?</p>
+          <div className='confirm-btns'>
+            <button
+              className='clear-btn'
+              onClick={() => {
+                setTasks([])
+                setConfirm(false)
+              }}>
+              Yes
+            </button>
+            <button
+              className='clear-btn'
+              onClick={() => setConfirm(false)}>
+              No
+            </button>
+          </div>
+        </div>
+      ) : (
+        null
+      )}
     </div>
   )
 }
