fefbf840c4a2fe2589c4c4df87aff139dcd90b97
gcpearse <glen.c.pearse@gmail.com>
2024-01-02 00:58:11 +0000

    adds ability to edit tasks

--- src/components/SingleTask.tsx
+++ src/components/SingleTask.tsx
@@ -1,6 +1,7 @@
 import { Task } from '../model'
 import { MdModeEdit, MdDelete, MdDone } from 'react-icons/md'
 import './styles.css'
+import { useEffect, useRef, useState } from 'react'
 
 interface Props {
   task: Task
@@ -10,35 +11,68 @@ interface Props {
 
 const SingleTask: React.FC<Props> = ({ task, tasks, setTasks }) => {
 
+  const [canEdit, setCanEdit] = useState<boolean>(false)
+  const [edit, setEdit] = useState<string>(task.task)
+
+  const inputRef = useRef<HTMLInputElement>(null)
+
+  useEffect(() => {
+    inputRef.current?.focus()
+  }, [canEdit])
+
+  const handleEdit = (event: React.FormEvent, id: number) => {
+    event.preventDefault()
+    setTasks(tasks.map((task) => {
+      if (task.id === id) {
+        return { ...task, task: edit }
+      } else {
+        return task
+      }
+    }))
+    setCanEdit(false)
+  }
+
   const handleDelete = (id: number) => {
-    setTasks(() => {
-      return tasks.filter((task) => {
-        return task.id !== id
-      })
-    })
+    setTasks(tasks.filter((task) => {
+      return task.id !== id
+    }))
   }
 
   const handleDone = (id: number) => {
-    setTasks(() => {
-      return tasks.map((task) => {
-        if (task.id === id) {
-          return { ...task, isComplete: !task.isComplete }
-        } else {
-          return task
-        }
-      })
-    })
+    setTasks(tasks.map((task) => {
+      if (task.id === id) {
+        return { ...task, isComplete: !task.isComplete }
+      } else {
+        return task
+      }
+    }))
   }
 
   return (
-    <form className='task-form'>
-      {!task.isComplete ? (
+    <form
+      className='task-form'
+      onSubmit={(event) => handleEdit(event, task.id)}
+      onBlur={(event) => handleEdit(event, task.id)}>
+      {canEdit ? (
+        <input
+          type='text'
+          className='task-text'
+          value={edit}
+          ref={inputRef}
+          onChange={(event) => setEdit(event.target.value)} />
+      ) : !task.isComplete ? (
         <span className='task-text'>{task.task}</span>
       ) : (
         <s className='task-text'>{task.task}</s>
       )}
       <div>
-        <span className='task-icon'>
+        <span
+          className='task-icon'
+          onClick={() => {
+            if (!canEdit && !task.isComplete) {
+              setCanEdit(!canEdit)
+            }
+          }}>
           <MdModeEdit />
         </span>
         <span
